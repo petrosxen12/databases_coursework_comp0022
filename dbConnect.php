d66279fb9c43b37f7e90e227cf19fe6e6544a8a8
Petros Xenofontos <petros.x12@gmail.com>
2020-02-03 18:31:07 +0000

    Adds example web app to run on AWAS

---
+++ dbConnect.php
@@ -0,0 +1,24 @@
+<?php
+    
+    $serverName = "dbserverxen.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "dbTesting", // update me
+        "Uid" => "xen@dbserverxen", // update me
+        "PWD" => "malakas123!@" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP (1000) * FROM [dbo].[Persons]";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo("<h3>");
+     echo ($row['LastName'] . " " . $row['FirstName'] . PHP_EOL);
+     echo("</h3>");
+
+    }
+    sqlsrv_free_stmt($getResults);
+    
+?>
