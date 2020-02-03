656da22af71a4fc168ae2bb6d1bcfe50042ea8ce
Petros Xenofontos <petros.x12@gmail.com>
2020-02-03 21:54:58 +0000

    Some css

--- index.php
+++ index.php
@@ -14,6 +14,42 @@
 
 <body>
 
+<style>
+.jumbotron {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  margin-bottom: 0;
+  background-color: #fff;
+}
+@media (min-width: 768px) {
+  .jumbotron {
+    padding-top: 6rem;
+    padding-bottom: 6rem;
+  }
+}
+
+.jumbotron p:last-child {
+  margin-bottom: 0;
+}
+
+.jumbotron-heading {
+  font-weight: 300;
+}
+
+.jumbotron .container {
+  max-width: 40rem;
+}
+
+footer {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+
+footer p {
+  margin-bottom: .25rem;
+}
+  </style>
+
 <div class="collapse bg-dark" id="navbarHeader">
     <div class="container">
       <div class="row">
