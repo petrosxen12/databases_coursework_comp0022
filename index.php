2ecb3b6e83f42a164aba9521552a9a8715f66c38
Petros Xenofontos <petros.x12@gmail.com>
2020-02-10 20:40:05 +0000

    Changes website title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Dashboard - SB Admin</title>
+    <title>Ebay Admin Dashboard</title>
     <link href="dist/css/styles.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
         crossorigin="anonymous" />
