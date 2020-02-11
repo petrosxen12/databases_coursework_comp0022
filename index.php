460fca4784411773720de8d051b2d69aec42cee1
Petros Xenofontos <petros.x12@gmail.com>
2020-02-11 11:11:46 +0000

    Title changes

--- index.php
+++ index.php
@@ -169,7 +169,7 @@
                         </div> -->
                     </div>
                     <div class="card mb-4">
-                        <div class="card-header"><i class="fas fa-table mr-1"></i>DataTable Example</div>
+                        <div class="card-header"><i class="fas fa-table mr-1"></i>Historical Data of Product</div>
                         <div class="card-body">
                             <div class="table-responsive">
                                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
