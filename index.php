59622566c6aebc86741cb4881c0896b112d73a56
Petros Xenofontos <petros.x12@gmail.com>
2020-02-11 10:07:57 +0000

    Adds background image to jumbotron

--- index.php
+++ index.php
@@ -94,11 +94,11 @@
             </nav>
         </div>
         <div id="layoutSidenav_content">
-            <div class="jumbotron">
-                <h1 class="display-4">Hello, world!</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <div class="jumbotron" id="top_jumbotron">
+                <h1 class="display-4">Monitor prices of cameras</h1>
+                <p class="lead">This web-app offers you the capability to monitor price changes and historical data of cameras</p>
                 <hr class="my-4">
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p>You can also view the best time to bid or if a certain listing is being sold at a good price. </p>
                 <p class="lead">
                     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
                 </p>
