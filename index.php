b69a4a9c4798e9979a893d4e33ee455ab803a0f6
Petros Xenofontos <petros.x12@gmail.com>
2020-03-02 22:47:34 +0000

    Fixes redirect

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once "dbConnect.php";
 
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    header("location: ../index.php");
+    header("location: /");
     exit;
 } else {
     header("location: /dist/login.php");
