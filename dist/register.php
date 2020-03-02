0117818448ecd159cf132f642a56cfca8c739aa7
Petros Xenofontos <petros.x12@gmail.com>
2020-03-02 20:27:27 +0000

    Adds working registration form

--- dist/register.php
+++ dist/register.php
@@ -125,26 +125,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                                         <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                                             <label>Username</label>
-                                            <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                                            <input type="text" name="username" class="form-control" value="<?php echo $username; ?>" placeholder="Enter username">
                                             <span class="help-block"><?php echo $username_err; ?></span>
                                         </div>
                                         <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                                             <label>Password</label>
-                                            <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                                            <span class="help-block"><?php echo $password_err; ?></span>
-                                        </div>
-
-                                        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                                            <label>Email</label>
-                                            <input type="email" name="email" class="form-control" value="<?php echo $email; ?>">
+                                            <input type="password" name="password" class="form-control" value="<?php echo $password; ?>" placeholder="Enter password">
                                             <span class="help-block"><?php echo $password_err; ?></span>
                                         </div>
 
                                         <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
                                             <label>Confirm Password</label>
-                                            <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                                            <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>" placeholder="Reenter password">
                                             <span class="help-block"><?php echo $confirm_password_err; ?></span>
                                         </div>
+
+                                        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                                            <label>Email</label>
+                                            <input type="email" name="email" class="form-control" value="<?php echo $email; ?>" placeholder="Enter email">
+                                            <span class="help-block"><?php echo $password_err; ?></span>
+                                        </div>
+
                                         <div class="form-group">
                                             <input type="submit" class="btn btn-primary" value="Submit">
                                             <input type="reset" class="btn btn-default" value="Reset">
@@ -180,41 +181,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="js/scripts.js"></script>
 </body>
-<!--     
-<body>
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
-            </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Email</label>
-                <input type="email" name="email" class="form-control" value="<?php echo $email; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-
-            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-                <span class="help-block"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-default" value="Reset">
-            </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
-        </form>
-    </div>
-</body> -->
 
 </html>
