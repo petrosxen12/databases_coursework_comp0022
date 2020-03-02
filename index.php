d9cbaa536c2edffda5aa4bb179f8129a3105ede7
Petros Xenofontos <petros.x12@gmail.com>
2020-03-02 23:28:26 +0000

    Change logout

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
                     <a class="dropdown-item" href="#">Settings</a><a class="dropdown-item" href="#">Activity Log</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="logout.php">Logout</a>
+                    <a class="dropdown-item" href="dist/logout.php">Logout</a>
                 </div>
             </li>
         </ul>
