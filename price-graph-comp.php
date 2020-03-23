8944c48c7052dcb44e526633120833507b0e68f2
Petros Xenofontos <petros.x12@gmail.com>
2020-03-23 20:47:44 +0000

    Removes unnecessary imports in deal cards

--- price-graph-comp.php
+++ price-graph-comp.php
@@ -19,7 +19,7 @@ function deal_card($chartid = "myChart", $title = "Default Title", $desc = "Defa
 <html>
 
 
-<head>
+<!-- <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -29,7 +29,7 @@ function deal_card($chartid = "myChart", $title = "Default Title", $desc = "Defa
     <link href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
-</head>
+</head> -->
 
 <body>
 
@@ -58,8 +58,8 @@ function deal_card($chartid = "myChart", $title = "Default Title", $desc = "Defa
 
 </body>
 
-<script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script> -->
+<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script> -->
 <!-- <script src="dist/js/scripts.js"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 <!-- <script src="dist/assets/demo/chart-area-demo.js"></script>
