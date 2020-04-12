48c54dbc96e5d8d5eda59df8a129ce91185a78d3
Petros Xenofontos <petros.x12@gmail.com>
2020-04-12 16:46:22 +0100

    Adds toggle for tracking items

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     <title>Ebay Admin Dashboard</title>
     <link href="dist/css/styles.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
 </head>
 
--- searchHandler.php
+++ searchHandler.php
@@ -14,6 +14,8 @@ function showLabels($blankcheckboxes, $auctst, $bnst, $numberofitems)
 {
     if ($blankcheckboxes) {
 
+        //Database call 
+
         // Deal items ==> Labels with charts
         include_once("price-graph-comp.php");
 
@@ -28,17 +30,29 @@ function showLabels($blankcheckboxes, $auctst, $bnst, $numberofitems)
 
         for ($i = 0; $i < $numberofitems; $i++) {
             echo <<<"EOT"
-                <div id="productcard" class="card mb-3" style="max-width: 80%;">
+                <div id="productcard" class="card mb-3" style="max-width: 70%;">
                     <div class="row no-gutters">
-                        <div class="col-md-4">
-                            <img src="$imgofitem" class="card-img" alt="...">
+                        <div class="col-md-4 stretched-link">
+                            <a href="#gotoitem" ><img src="$imgofitem" class="card-img" alt="..."></a>
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
-                                <h5 class="card-title"> $searchstr $badge  </h5>
+                                <h5 class="card-title"> $searchstr $badge <a onclick=' 
+                                var x = document.getElementById("unTrackedItem$i");
+                                var y = document.getElementById("trackItem$i");
+
+                                if (x || y) {
+                                    if (x.style.display === "none") {
+                                        x.style.display = "inline-block";
+                                        y.style.display = "none";
+                                        } else {
+                                            x.style.display = "none";
+                                            y.style.display = "inline-block"; 
+                                        }
+                                }' 
+                                style="padding-left: 2rem;" href="#testing" class="card-link"><i id="unTrackedItem$i" class="far fa-star"></i><i id="trackItem$i" style="display:none;" class="fas fa-star"></i></a> </h5>
                                 <p class="card-text">$itemdescription</p>
                                 <p class="card-text"><small class="text-muted">Last updated $updatedtime mins ago</small></p>
-                                <a href="#" class="stretched-link"></a>
                             </div>
                         </div>
                     </div>
@@ -47,6 +61,7 @@ function showLabels($blankcheckboxes, $auctst, $bnst, $numberofitems)
         }
     }
 }
+
 
 function showErrorBadge($blankcheckboxes)
 {
