c1d9f7f04b7b4e6fd5fcda914beb941405e1c51a
elena-aleksieva <ealeksieva98@gmail.com>
2020-04-16 15:24:20 +0300

    searching by string

---
+++ select-item.php
@@ -0,0 +1,39 @@
+<?php 
+include "connect-to-database.php";
+
+function searchItem($conn, $keyword) {
+    $sql = "EXEC [dbo].[SearchItemByKeyword] @keyword = ?";
+    //$output = [];
+    $params = array(
+        array($keyword, SQLSRV_PARAM_IN)
+        //array(&$output, SQLSRV_PARAM_OUT)
+    );
+    $stmt = sqlsrv_prepare($conn, $sql, $params);
+    if($stmt) {  
+        echo "Statement prepared.\n";  
+    }  
+    else {  
+        echo "Error in preparing statement.\n";  
+        die( print_r( sqlsrv_errors(), true));  
+    } 
+    
+    if(sqlsrv_execute( $stmt)) {  
+        echo "Statement executed.\n";  
+    }  
+    else  
+    {  
+        echo "Error in executing statement.\n";  
+        die( print_r( sqlsrv_errors(), true));  
+    }
+
+    /* Retrieve each row as an associative array and display the results.*/  
+    while( $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))  
+    {  
+        echo $row['Title']."\n";  
+    }   
+ 
+}
+
+$connection = connectToDB();
+searchItem($connection, "mint");
+?>
