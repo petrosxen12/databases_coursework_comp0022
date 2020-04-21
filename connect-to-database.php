e8a0a0fb12d0bffbb37b5c66edd7a68e74d62804
Petros Xenofontos <petros.x12@gmail.com>
2020-04-21 23:51:05 +0100

    Env vars in db connections

--- connect-to-database.php
+++ connect-to-database.php
@@ -1,20 +1,26 @@
 <?php
 
-function connectToDB() {
+$dbpassword = getenv('APPSETTING_dbpassword');
+
+
+function connectToDB()
+{
+    if (getenv('APPSETTING_env') == true) {
+        $dbpasswordazure = getenv('APPSETTING_dbpassword');
+        $pass = $dbpasswordazure;
+    }
+
     try {
         $serverName = "tcp:dbserverxen.database.windows.net,1433";
-        $connectionOptions = array("UID" => "xen", "PWD" => "malakas123!@", "Database" => "Ebay", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-        $conn = sqlsrv_connect($serverName, $connectionOptions);  
-        if($conn == false)  
-            die(FormatErrors(sqlsrv_errors())); 
+        $connectionOptions = array("UID" => "xen", "PWD" => $pass, "Database" => "Ebay", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if ($conn == false)
+            die(FormatErrors(sqlsrv_errors()));
         else {
-            echo("Connection Successful!\n");
+            echo ("Connection Successful!\n");
         }
-    }
-    catch (Exception $e) {
-        echo("Error!");
+    } catch (Exception $e) {
+        echo ("Error!");
     }
     return $conn;
 }
-
-?>
--- dbConnect.php
+++ dbConnect.php
@@ -17,11 +17,21 @@
     //     // print("Connected succesfully.");
     // }
 
+    $dbpasswordazure = getenv('APPSETTING_dbpassword');
+    $dbpasslocal = getenv('GROUP2DBPASS');
+
+    echo $dbpasslocal;
+
     function connectToDB()
     {
+        if (getenv('APPSETTING_env') == true) {
+            $dbpasswordazure = getenv('APPSETTING_dbpassword');
+            $pass = $dbpasswordazure;
+        }
+
         try {
             $serverName = "tcp:dbserverxen.database.windows.net,1433";
-            $connectionOptions = array("UID" => "xen", "PWD" => "malakas123!@", "Database" => "Ebay", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+            $connectionOptions = array("UID" => "xen", "PWD" => $pass, "Database" => "Ebay", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
             $conn = sqlsrv_connect($serverName, $connectionOptions);
             if ($conn == false)
                 die(FormatErrors(sqlsrv_errors()));
